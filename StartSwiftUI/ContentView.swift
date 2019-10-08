64b5d4f510b276e35fde822f3e2bc4cb81ee898e
Milkyo <dp221125@naver.com>
2019-10-08 13:44:24 +0900

    Initial Commit

---
+++ StartSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StartSwiftUI
+//
+//  Created by SeokHo on 2019/10/08.
+//  Copyright © 2019 SeokHo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
